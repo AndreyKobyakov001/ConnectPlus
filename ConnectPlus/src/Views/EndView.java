91b0001a123f8a376227fbe06538e21f874d5e60
Christopher GIANNOULIS <christopher.giannoulis@mail.utoronto.ca>
2023-11-24 23:39:43 -0500

    adding end game screen view for now redirecting back to signup view, need to add facebook share thing but it turns out to be harder than expected

---
+++ ConnectPlus/src/Views/EndView.java
@@ -0,0 +1,79 @@
+package Views;
+
+import Entities.User;
+import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class EndView extends JPanel implements ActionListener, PropertyChangeListener {
+    private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel();
+
+    private JButton share = new JButton("Share");
+
+    private JLabel elo = new JLabel("Your new elo is:" );
+
+    private JButton playagain = new JButton("Play Again");
+
+    private JButton exit = new JButton("Exit");
+
+    private JLabel goodgame = new JLabel("Create new Account");
+
+    public EndView(User user){
+        goodgame.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Create new Account");
+        frame.pack();
+        frame.setVisible(true);
+
+        exit.addActionListener(
+                new ActionListener()    {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        frame.dispose();
+                        new SignupView();
+                    }
+                }
+        );
+
+        playagain.addActionListener(
+                new ActionListener()    {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        frame.dispose();
+                        new SignupView();
+                    }
+                }
+        );
+        share.addActionListener(
+                new ActionListener()    {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                    }
+                }
+        );
+
+        panel.add(elo);
+        panel.add(playagain);
+        panel.add(exit);
+        panel.add(share);
+        }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+}
