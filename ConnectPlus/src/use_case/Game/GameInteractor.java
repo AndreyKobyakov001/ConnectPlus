f7aef66314876d4c5eac4ab1862f7999e7ddaa1c
Andrey <andreykobyakov001@gmail.com>
2023-11-26 18:04:44 -0500

    Per comments, made CheckWinInput into interface and Interactor implements  it

--- ConnectPlus/src/use_case/Game/GameInteractor.java
+++ ConnectPlus/src/use_case/Game/GameInteractor.java
@@ -1,4 +1,5 @@
 package use_case.Game;
 
 public class GameInteractor {
+
 }
--- ConnectPlus/src/use_case/check_win/CheckWinInputBoundary.java
+++ ConnectPlus/src/use_case/check_win/CheckWinInputBoundary.java
@@ -2,26 +2,35 @@ package use_case.check_win;
 
 import Entities.Board;
 
-public class CheckWinInputBoundary {
-    private char[][] board;
-    private int piecesToConnect;
-    private char playerSymbol;
-
-    public CheckWinInputBoundary(Board board, int piecesToConnect, char playerSymbol) {
-        this.board = board.getBoard();
-        this.piecesToConnect = piecesToConnect;
-        this.playerSymbol = playerSymbol;
-    }
-
-    public int getPiecesToConnect() {
-        return piecesToConnect;
-    }
-
-    public char getPlayerSymbol() {
-        return playerSymbol;
-    }
-
-    public char[][] getBoard() {
-        return board;
-    }
+public interface CheckWinInputBoundary {
+    int getPiecesToConnect();
+    char getPlayerSymbol();
+    char[][] getBoard();
 }
+
+
+//public class CheckWinInputBoundary {
+
+//    private final char[][] board;
+//    private final int piecesToConnect;
+//    private final char playerSymbol;
+//
+//    public CheckWinInputBoundary(Board board, int piecesToConnect, char playerSymbol) {
+//        this.board = board.getBoard();
+//        this.piecesToConnect = piecesToConnect;
+//        this.playerSymbol = playerSymbol;
+//    }
+//
+//    public int getPiecesToConnect() {
+//        return piecesToConnect;
+//    }
+//
+//    public char getPlayerSymbol() {
+//        return playerSymbol;
+//    }
+//
+//    public char[][] getBoard() {
+//        return board;
+//    }
+//}
+
