764427e9b6b94683ed941149c730a0e4496f2156
Andrey <andreykobyakov001@gmail.com>
2023-11-26 16:22:53 -0500

    Finished CheckWin use case

--- ConnectPlus/src/use_case/check_win/CheckWinInputBoundary.java
+++ ConnectPlus/src/use_case/check_win/CheckWinInputBoundary.java
@@ -1,4 +1,27 @@
 package use_case.check_win;
 
+import Entities.Board;
+
 public class CheckWinInputBoundary {
+    private char[][] board;
+    private int piecesToConnect;
+    private char playerSymbol;
+
+    public CheckWinInputBoundary(Board board, int piecesToConnect, char playerSymbol) {
+        this.board = board.getBoard();
+        this.piecesToConnect = piecesToConnect;
+        this.playerSymbol = playerSymbol;
+    }
+
+    public int getPiecesToConnect() {
+        return piecesToConnect;
+    }
+
+    public char getPlayerSymbol() {
+        return playerSymbol;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
 }
--- ConnectPlus/src/use_case/check_win/CheckWinInteractor.java
+++ ConnectPlus/src/use_case/check_win/CheckWinInteractor.java
@@ -1,4 +1,19 @@
 package use_case.check_win;
 
+import Entities.WinChecker;
+
 public class CheckWinInteractor {
+    private final CheckWinInputBoundary input;
+    private final CheckWinOutputBoundary output;
+
+    public CheckWinInteractor(CheckWinInputBoundary input, CheckWinOutputBoundary output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void execute() {
+        WinChecker checkWin = new WinChecker(input.getPiecesToConnect(), input.getBoard());
+        boolean isWin = checkWin.checkWin(input.getPlayerSymbol());
+//   #TODO: make some indication of victory, when it is present.
+    }
 }
